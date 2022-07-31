93004031c96e876d70f7d78f87037d2eead35ac3
Egg <adrianjimenez1950@gmail.com>
2022-07-30 23:23:39 -0400

    added input readability from form to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,23 @@ var createTaskHandler = function(event) {
     
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // li created with class task-item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // div created with class task-info. contains html that displays the taskNameInput
+    // as well as the taskTypeInput
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+    
+    // appends div taskInfoEl to the aforementioned list item ListItemEl
+    listItemEl.appendChild(taskInfoEl);
+
+    // appends list item to the UL already on the html document
     tasksToDoEl.appendChild(listItemEl);
 };
 
